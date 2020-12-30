fd9ebb721198045ce341ec011325094eba26f780
Kuanju Chen <kuanju0919@yahoo.com>
2020-12-29 16:33:14 -0800

    Change the conda location and add activate in the nv command

--- neovim/.config/nvim/lua/defaults.lua
+++ neovim/.config/nvim/lua/defaults.lua
@@ -1,7 +1,7 @@
 
 -- some constant
 return {
-  lsp_condaenv_bin = vim.loop.os_homedir()..'/repos/dev-env-ansible/env/bin/',
+  lsp_condaenv_bin = vim.loop.os_homedir()..'/miniconda3/envs/dev_env_ansible/bin/',
   pses_bundle_path = vim.env.HOME..'/repos/PowerShellEditorServices/module'
 }
 
