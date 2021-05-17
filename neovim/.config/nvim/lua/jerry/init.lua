c34336fe8b0aeca9bca798fbf4a73bbd93d6f884
Kuanju Chen <kuanju0919@yahoo.com>
2021-05-17 01:02:03 -0700

    remove telescope c-v override, no longer used

--- neovim/.config/nvim/lua/jerry/init.lua
+++ neovim/.config/nvim/lua/jerry/init.lua
@@ -63,7 +63,6 @@ require('telescope').setup({
         mappings = {
             i = {
                 ["<C-q>"] = actions.send_to_qflist,
-                ["<C-v>"] = actions.select_default,
                 ["<C-h>"] = function(prompt_nr)
                     actions.select_vertical(prompt_nr)
                     vim.cmd [[ FSNoToggle ]]
