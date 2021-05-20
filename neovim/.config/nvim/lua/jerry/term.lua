0c5d0e07567ee7b93bcee0e345a5ba2ae460a40a
Kuanju Chen <kuanju0919@yahoo.com>
2021-05-20 00:07:33 -0700

    add send command

--- neovim/.config/nvim/lua/jerry/term.lua
+++ neovim/.config/nvim/lua/jerry/term.lua
@@ -1,33 +1,63 @@
 
 local vim = vim
 
-local M = {}
+local filter = vim.tbl_filter
+local map = vim.tbl_map
+local keys = vim.tbl_keys
+local contains = vim.tbl_contains
 
-M.get_term_id_list = function()
-  return vim.tbl_map(function(buf)
-      return buf.variables.terminal_job_id
-    end, vim.tbl_filter(function(buf)
-      if vim.tbl_contains(vim.tbl_keys(buf), 'variables') then
-        if vim.tbl_contains(vim.tbl_keys(buf.variables), 'terminal_job_id') then
-          return true
-        end
-      end
-      return false
-  end, vim.fn.getbufinfo()))
+local function get_term_ids()
+  local bufs = vim.fn.getbufinfo()
+
+  local bufs_vars = filter(function (buf)
+    return contains(keys(buf), 'variables')
+  end, bufs)
+
+  local bufs_termid = filter(function (buf)
+    return contains(keys(buf.variables), 'terminal_job_id')
+  end, bufs_vars)
+
+  return map(function (buf) return buf.variables.terminal_job_id end, bufs_termid)
 end
 
-M.get_term_id = function()
-  local terms = M.get_term_id_list()
+local function get_term_id(bufidx)
+  bufidx = bufidx or 1
+  local terms = get_term_ids()
+
   if #terms < 1 then
+    if bufidx > 1 then
+      error('bufidx cannot be more than 1, only 1 terminal will be created')
+    end
+
     local current_id = vim.fn.bufnr()
     vim.cmd [[ terminal ]]
     vim.api.nvim_set_current_buf(current_id)
-    return M.get_term_id_list()[1]
+    return get_term_ids()[bufidx]
   else
-    return terms[1]
+    if bufidx > #terms then
+      error(string.format('bufidx cannot be more than %d', #terms))
+    end
+
+    return terms[bufidx]
   end
 end
 
-return M
+local function send(cmd, opt)
+  opt = opt or {}
+  local id = opt.term_id or get_term_id(opt.term_idx)
+
+  if cmd:sub(-string.len(cmd)) ~= '\n' then
+    cmd = cmd .. '\n'
+  end
+
+  local ok, _ = pcall(vim.api.nvim_chan_send, id, cmd)
+  if not ok then
+    error(string.format('channel id %d is not valid, availables are %s', id, vim.inspect(get_term_ids())))
+  end
+end
+
+return {
+  send = send
+}
 
 -- vim:et sw=2 ts=2 sts=2
