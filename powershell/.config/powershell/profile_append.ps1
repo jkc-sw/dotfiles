1e44bc85b57024c88684e34e6ad5dee0d965cb4a
Kuanju Chen <kuanju0919@yahoo.com>
2021-08-11 02:27:55 -0700

    shell refactor + add zoxide

--- powershell/.config/powershell/profile_append.ps1
+++ powershell/.config/powershell/profile_append.ps1
@@ -2,5 +2,9 @@
 
 # Set the VI mode
 Set-PSReadlineOption -EditMode vi
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
 Invoke-Expression (& starship init powershell)
 
