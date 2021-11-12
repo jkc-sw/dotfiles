c500d5e97814020567a618beed70cb88afcc4632
Kuanju Chen <kuanju0919@yahoo.com>
2021-11-12 14:49:48 -0800

    update psreadline to have auto suggestion

--- powershell/.config/powershell/profile_append.ps1
+++ powershell/.config/powershell/profile_append.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/pwsh
 
 # Set the VI mode
-Set-PSReadlineOption -EditMode vi
+Set-PSReadlineOption -EditMode vi -PredictionSource History
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
