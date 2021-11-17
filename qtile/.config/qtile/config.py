bb982cf9d752c45d01aa25ce19dd62e04c26069e
Kuanju Chen <kuanju0919@yahoo.com>
2021-11-16 18:26:41 -0800

    add comment

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -77,7 +77,7 @@ keys.extend([
 
     add_key([mod, "shift",   ""],        "n",      "Reset all window sizes",             lazy.layout.normalize()),
     add_key([mod, "",        ""],        "t",      "Toggle the window to floating",      lazy.window.toggle_floating()),
-    add_key([mod, "shift",   ""],        "t",      "Toggle the window to floating",      put_all_to_tile()),
+    add_key([mod, "shift",   ""],        "t",      "Put all windows back to tile",       put_all_to_tile()),
     add_key([mod, "",        ""],        "a",      "Go back to the last group",          lazy.screen.toggle_group()),
     add_key([mod, "",        ""],        "f",      "Toggle full screen",                 lazy.window.toggle_fullscreen()),
     add_key([mod, "",        ""],        "n",      "To the next group",                  lazy.screen.next_group()),
