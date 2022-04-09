e604551f05b2912b8763d3ed04c66101aa678b01
Kuanju Chen <kuanju0919@yahoo.com>
2022-04-08 23:21:06 -0700

    update the sources to search the entire file

--- neovim/.config/nvim/lua/jerry/sourcer.lua
+++ neovim/.config/nvim/lua/jerry/sourcer.lua
@@ -6,13 +6,13 @@ local M = {}
 
 --[[
 SOURCE_THESE_VIMS_START
-nnoremap <leader>ti <cmd>lua R('jerry.sourcer').vim_sourcer('SOURCE_THESE_VIMS_START', 'SOURCE_THESE_VIMS_END')<cr>
+nnoremap <leader>ne <cmd>lua R('jerry.sourcer').vim_sourcer('TSOURCE_THESE_VIMS_START', 'TSOURCE_THESE_VIMS_END')<cr>
 let @h="yoP('\<c-r>\" = ' .. vim.inspect(\<c-r>\"))\<esc>+"
 echom 'Sourced'
 SOURCE_THESE_VIMS_END
 --]]
 
--- @brief Get a function te search the current screensection for a matching pattern and source using sourcer
+-- @brief Get a function te search the entire file for a matching pattern closest to the cursor and source using sourcer
 -- @param sourcer (function(x) -> nil) - A function to source the block of text
 --        function(x) -> nil
 --        Arguments:
@@ -22,7 +22,7 @@ SOURCE_THESE_VIMS_END
 --        Returns:
 --          bool - True to skip this source, False to continue the rest of the function
 local function new_sourcer(sourcer, excluders)
-  -- @brief Search the current screensection for a matching pattern and source that as vimscript
+  -- @brief Search the entire file for a matching pattern closest to the cursor and source that as vimscript
   -- @param startpat (str) - The pattern used for searching the start of the block
   -- @param endpat (str) - The pattern used for searching the end of the block
   return function(startpat, endpat)
@@ -34,9 +34,13 @@ local function new_sourcer(sourcer, excluders)
     end
 
     -- Get the lines currently visible
-    local startrow = vim.fn.line('w0')
-    local endrow = vim.fn.line('w$')
+    local startrow = 0 -- vim.fn.line('0')
+    local endrow = -1 -- vim.fn.line('$')
     local vlines = vim.api.nvim_buf_get_lines(0, startrow, endrow, true)
+    -- P(vlines[1]..'1')
+    -- P(vlines[2]..'2')
+    -- P(vlines[#vlines - 1]..'$-1')
+    -- P(vlines[#vlines]..'$')
 
     -- Get the current cursor location
     local curln, _ = unpack(vim.api.nvim_win_get_cursor(0))
@@ -54,24 +58,27 @@ local function new_sourcer(sourcer, excluders)
       -- if we have found the startpat
       if foundstartpat then
         if li:find(endpat) then
-          -- Calculate the distance and store the result
           endln = ln + startrow
-          dist = math.min(math.abs(startln - curln), math.abs(endln - curln))
-          -- P('endln = ' .. vim.inspect(endln))
-          -- P('startln = ' .. vim.inspect(startln))
-          -- P('dist = ' .. vim.inspect(dist))
-          -- P('lastdist = ' .. vim.inspect(lastdist))
-          -- P('lastblk = ' .. vim.inspect(lastblk))
-          -- P('blk = ' .. vim.inspect(blk))
-          -- If first time or distance is shorter, we will store the result
-          if lastdist == -1 or dist < lastdist then
-            lastdist = dist
-            lastblk = blk
-            blk = {}
+          -- only do the following if there are lines
+          if (endln - startln) > 1 then
+            -- Calculate the distance and store the result
+            dist = math.min(math.abs(startln - curln), math.abs(endln - curln))
+            P('endln = ' .. vim.inspect(endln))
+            P('startln = ' .. vim.inspect(startln))
+            P('dist = ' .. vim.inspect(dist))
+            P('lastdist = ' .. vim.inspect(lastdist))
+            P('lastblk = ' .. vim.inspect(lastblk))
+            P('blk = ' .. vim.inspect(blk))
+            -- If first time or distance is shorter, we will store the result
+            if lastdist == -1 or dist < lastdist then
+              lastdist = dist
+              lastblk = blk
+              blk = {}
+            end
+            foundstartpat = false
+            -- -- exit the loop
+            -- break
           end
-          foundstartpat = false
-          -- -- exit the loop
-          -- break
         end
       end
 
@@ -82,9 +89,12 @@ local function new_sourcer(sourcer, excluders)
 
       -- if we have found the startpat
       if li:find(startpat) then
-        -- store the start line
-        startln = ln + startrow
-        foundstartpat = true
+        if not li:find(endpat) then
+          -- only when they are not on the same line
+          -- store the start line
+          startln = ln + startrow
+          foundstartpat = true
+        end
       end
     end
 
@@ -138,6 +148,18 @@ M.vim_sourcer = new_sourcer(M.eval_vimscript, {M.initfile_excluder})
 M.lua_sourcer = new_sourcer(M.eval_lua, {M.initfile_excluder})
 
 return M
+
+--[[
+TSOURCE_THESE_VIMS_START
+echom "Sourced 1"
+TSOURCE_THESE_VIMS_END
+--]]
+
+--[[
+TSOURCE_THESE_VIMS_START
+echom "Sourced 2"
+TSOURCE_THESE_VIMS_END
+--]]
 
 -- vim:et sw=2 ts=2 sts=2
 
