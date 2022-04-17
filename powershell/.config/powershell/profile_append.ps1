e430fd5ebbca781111c649eaf5631b71a9399fca
Kuanju Chen <kuanju0919@yahoo.com>
2022-04-16 17:53:13 -0700

    change the zoxide to c, since pinky hurts

--- powershell/.config/powershell/profile_append.ps1
+++ powershell/.config/powershell/profile_append.ps1
@@ -4,7 +4,7 @@
 Set-PSReadlineOption -EditMode vi -PredictionSource History
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
+    (zoxide init --cmd 'c' --hook $hook powershell) -join "`n"
 })
 Invoke-Expression (& starship init powershell)
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
