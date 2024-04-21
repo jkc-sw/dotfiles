9c3fcdbe2f38f1021b0fd87f4e209767ddc2e8a1
Kuanju Chen <kuanju.chen@mksinst.com>
2024-04-20 21:55:19 -0700

    WIP lazy should be working

--- neovim/.config/nvim/lua/jerry/init.lua
+++ neovim/.config/nvim/lua/jerry/init.lua
@@ -1,19 +1,4 @@
 
--- Load the plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
-
 require('jerry.helpers')
 
 S = function(...) require('jerry.term').send(...) end
@@ -64,23 +49,6 @@ cmp.setup{
 
 local neogit = require('neogit')
 neogit.setup{}
-
-require('nvim-treesitter.configs').setup{
-  ensure_installed = 'all',
-  sync_install = false,
-  auto_install = false,
-  context_commentstring = {
-    enable = false,
-  },
-  highlight = {
-    enable = true,
-    disable = { 'cpp' }
-  },
-  indent = {
-    enable = false,
-    disable = { 'python' }
-  }
-}
 
 require('nvim_context_vt').setup({
   -- -- Override default virtual text prefix
