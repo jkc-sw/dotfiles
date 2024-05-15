35a421c5798fbd3ae7e30afb72c7078a14cdc909
Kuanju Chen <kuanju.chen@mksinst.com>
2024-05-14 21:50:33 -0700

    Disable lazy.nvim change detect
    Change detection would freeze my neovim instances.

--- neovim/.config/nvim/lua/jerry/plugins.lua
+++ neovim/.config/nvim/lua/jerry/plugins.lua
@@ -12,6 +12,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = {
+    enabled = false,
+    notify = false,
+  }
+})
 
 -- vim:et sw=2 ts=2 sts=2
