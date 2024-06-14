01a4de0afa191979752b97613f2f1b5da7076033
Kuanju Chen <kuanju.chen@mksinst.com>
2024-06-13 20:16:25 -0700

    Enable pwsh es work

--- neovim/.config/nvim/pack/perforce/start/perforce/lua/perforce/lsp.lua
+++ neovim/.config/nvim/pack/perforce/start/perforce/lua/perforce/lsp.lua
@@ -1,6 +1,6 @@
 
 local jerry_lsp = require('jerry.lsp.config')
-if vim.fn.executable('power_es_work.sh') == 0 then
+if vim.fn.executable('power_es_work.sh') == 1 then
   jerry_lsp.setup_each_lsp('powershell_es', {
     cmd = {'power_es_work.sh'}
   })
