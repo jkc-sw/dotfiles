8b77dd946162bb68aa9ae5d70bec69892a690e8e
Kuanju Chen <kaunju0919@yahoo.com>
2024-08-20 20:20:26 -0700

    add wezterm conf

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrainsMonoMedium Nerd Font Mono'
+config.font_size = 14.0
+config.enable_scroll_bar = true
+
+return config
+
