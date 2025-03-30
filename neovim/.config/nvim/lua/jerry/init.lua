b9675051cebd12b6ee542b6ac81e10b4280ab7ed
Kuanju Chen <kuanju0919@yahoo.com>
2025-03-30 18:38:22 +0000

    neovim: Chose render-markdown over markview

--- neovim/.config/nvim/lua/jerry/init.lua
+++ neovim/.config/nvim/lua/jerry/init.lua
@@ -1,5 +1,10 @@
 require('jerry.global-funcs')
 
+local work_dir = vim.uv.os_homedir() .. '/.config/nvim'
+if vim.uv.fs_stat(work_dir) then
+  vim.opt.packpath:prepend(work_dir)
+end
+
 require('jerry.plugins-cfg.lualine')
 require('jerry.plugins-cfg.colorful-menu')
 require('jerry.plugins-cfg.blink-cmp')
@@ -10,7 +15,7 @@ require('jerry.plugins-cfg.nvim_context_vt')
 require('jerry.plugins-cfg.telescope')
 require('jerry.plugins-cfg.colorizer')
 -- require('jerry.plugins-cfg.markview')
--- require('jerry.plugins-cfg.render-markdown')
+require('jerry.plugins-cfg.render-markdown')
 
 require('jerry.markdown').setup()
 require('jerry.lsp.config').setup()
--- neovim/.config/nvim/lua/jerry/plugins-cfg/markview.lua
+++ neovim/.config/nvim/lua/jerry/plugins-cfg/markview.lua
@@ -1,1 +1,11 @@
-require('markview').setup()
+require('markview').setup {
+  preview = {
+    icon_provider = 'mini',
+    hybrid_modes = {'n'}
+  },
+  markdown = {
+    code_blocks = {
+      label_direction = 'left'
+    }
+  }
+}
