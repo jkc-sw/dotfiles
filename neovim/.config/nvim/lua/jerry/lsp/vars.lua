0f029e4aeec3091f8174419103e2f6bdf6ba4cf5
Kuanju Chen <kuanju0919@yahoo.com>
2025-03-30 05:36:43 +0000

    neovim: remove unused lsp files for custom powershell es

--- neovim/.config/nvim/lua/jerry/lsp/vars.lua
+++
@@ -1,9 +0,0 @@
-
--- some constant
-return {
-  luals_repos = (vim.uv or vim.loop).os_homedir()..'/repos/lua-language-server/',
-  lsp_condaenv_bin = (vim.uv or vim.loop).os_homedir()..'/miniconda3/envs/dev_env_ansible/bin/',
-  pses_bundle_path = vim.env.HOME..'/repos/PowerShellEditorServices/module'
-}
-
--- vim:et ts=2 sw=2
