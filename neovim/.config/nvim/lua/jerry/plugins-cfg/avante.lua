b8e7f6fe82849dac621c65ba3e8cc86dd278784b
Kuanju Chen <kuanju0919@yahoo.com>
2025-04-15 05:59:49 +0000

    neovim: avante add more models

--- neovim/.config/nvim/lua/jerry/plugins-cfg/avante.lua
+++ neovim/.config/nvim/lua/jerry/plugins-cfg/avante.lua
@@ -3,10 +3,19 @@ require("avante").setup({
   provider = "azure",
   azure = {
     endpoint = "https://openai-neovim.openai.azure.com/", -- example: "https://<your-resource-name>.openai.azure.com"
-    deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+
+    deployment = "o3-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
     api_version = "2024-12-01-preview",
+
+    -- deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+    -- api_version = "2024-12-01-preview",
+    -- temperature = 0,
+
+    -- deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+    -- api_version = "2024-12-01-preview",
+    -- temperature = 0,
+
     timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    temperature = 0,
     max_completion_tokens = 16384, -- Increase this to include reasoning tokens (for reasoning models)
     reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
   },
