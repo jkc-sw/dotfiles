2e6ca4397f2d33d1343608b637023063e1be9d30
Kuanju Chen <kuanju0919@yahoo.com>
2025-04-16 23:44:05 +0000

    neovim: add basic codecompanion config

--- neovim/.config/nvim/lua/jerry/init.lua
+++ neovim/.config/nvim/lua/jerry/init.lua
@@ -16,6 +16,7 @@ require('jerry.plugins-cfg.telescope')
 require('jerry.plugins-cfg.colorizer')
 -- require('jerry.plugins-cfg.markview')
 require('jerry.plugins-cfg.render-markdown')
+require('jerry.plugins-cfg.codecompanion')
 
 require('jerry.markdown').setup()
 require('jerry.lsp.config').setup()
