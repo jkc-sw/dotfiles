6107c3ae603d9e4738922395d9fd65a0452a1cd4
Kuanju Chen <kuanju0919@yahoo.com>
2025-07-16 01:38:13 +0000

    neovim: use powershell-editor-service from nixpkgs

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/lsp/config.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/lsp/config.lua
@@ -218,7 +218,7 @@ M.setup = function()
     })
   else
     setup_each_lsp('powershell_es', {
-      cmd = { 'powershell_es', '-Stdio' }
+      cmd = { 'powershell-editor-services', '-Stdio' }
     })
   end
 
