c1f09599e1f2ebfbce4e257360173fcd0ed8e49f
Kuanju Chen <kuanju0919@yahoo.com>
2025-07-19 23:59:51 +0000

    neovim: avante use work token

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
@@ -2,14 +2,14 @@ require("avante_lib").load()
 require("avante").setup({
   provider = "azure",
   azure = {
-    endpoint = "https://openai-neovim.openai.azure.com/", -- example: "https://<your-resource-name>.openai.azure.com"
+    endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
+
+    deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+    api_version = "2025-01-01-preview",
+    temperature = 0,
 
     -- deployment = "o3-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
     -- api_version = "2024-12-01-preview",
-
-    deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-    api_version = "2024-12-01-preview",
-    temperature = 0,
 
     -- deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
     -- api_version = "2024-12-01-preview",
