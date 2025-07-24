5b1b4b61219dc01c30ae71977d6f452a3882d166
Kuanju Chen <kuanju0919@yahoo.com>
2025-07-24 02:43:48 +0000

    neovim: avante.nvim configuration migrated to latest version

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
@@ -1,23 +1,27 @@
 require("avante_lib").load()
 require("avante").setup({
   provider = "azure",
-  azure = {
-    endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
+  providers = {
+    azure = {
+      endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
 
-    deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-    api_version = "2025-01-01-preview",
-    temperature = 0,
+      deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      api_version = "2025-01-01-preview",
 
-    -- deployment = "o3-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-    -- api_version = "2024-12-01-preview",
+      -- deployment = "o3-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      -- api_version = "2024-12-01-preview",
 
-    -- deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-    -- api_version = "2024-12-01-preview",
-    -- temperature = 0,
+      -- deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      -- api_version = "2024-12-01-preview",
+      -- temperature = 0,
 
-    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    max_completion_tokens = 16384, -- Increase this to include reasoning tokens (for reasoning models)
-    reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      extra_request_body = {
+        -- temperature = 0,
+        reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        max_completion_tokens = 16384, -- Increase this to include reasoning tokens (for reasoning models)
+      },
+    },
   },
   behaviour = {
     enable_claude_text_editor_tool_mode = false,
