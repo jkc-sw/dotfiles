bb319757fa7078ea253cbced38222eb042caa983
Kuanju Chen <kuanju0919@yahoo.com>
2025-08-26 23:07:46 +0000

    add more models

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/avante.lua
@@ -3,23 +3,26 @@ require("avante").setup({
   provider = "azure",
   providers = {
     azure = {
-      api_key_name = {'pas', 'mks/services/openai-it/key1'},
 
-      endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
-      deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-      api_version = "2025-01-01-preview",
+      api_key_name = {'pas', 'mks/services/openai-it/key2'},
+      endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
+      deployment = "gpt-5-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      api_version = "2025-04-01-preview",
 
-      -- deployment = "o3-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-      -- api_version = "2024-12-01-preview",
+      -- api_key_name = {'pas', 'mks/services/openai-it/key2'},
+      -- endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
+      -- deployment = "o4-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      -- api_version = "2025-01-01-preview",
 
-      -- deployment = "gpt-4o-mini", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
-      -- api_version = "2024-12-01-preview",
-      -- temperature = 0,
+      -- api_key_name = {'pas', 'mks/services/openai-it/key1'},
+      -- endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com", -- example: "https://<your-resource-name>.openai.azure.com"
+      -- deployment = "gpt-4o", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+      -- api_version = "2025-01-01-preview",
 
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
       extra_request_body = {
-        -- temperature = 0,
-        reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        temperature = 1, -- gpt-5-mini
+        reasoning_effort = "low", -- low|medium|high, only used for reasoning models
         max_completion_tokens = 16384, -- Increase this to include reasoning tokens (for reasoning models)
       },
     },
--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/codecompanion.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/codecompanion.lua
@@ -11,18 +11,38 @@ require("codecompanion").setup({
         -- https://codecompanion.olimorris.dev/getting-started.html
         -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/adapters/azure_openai.lua
         env = {
-          endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com",
-          api_key = 'cmd:pas mks/services/openai-it/key1',
+
+          -- -- gpt-5-mini
+          -- api_key = 'cmd:pas mks/services/openai-it/key2',
+          -- endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
+          -- api_version = "2025-04-01-preview",
+
+          -- o4-mini
+          api_key = 'cmd:pas mks/services/openai-it/key2',
+          endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
           api_version = "2025-01-01-preview",
+
+          -- -- gpt-4o
+          -- api_key = 'cmd:pas mks/services/openai-it/key1',
+          -- endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com",
+          -- api_version = "2025-01-01-preview",
+
         },
         schema = {
           model = {
-            default = "gpt-4o",
-            choices = {
-              "gpt-4o",
-              -- ["o3-mini"] = { opts = { can_reason = true } },
-              -- "gpt-4o-mini",
-            }
+            default = "gpt-5-mini",
+            choices = { "gpt-5-mini" },
+
+            -- default = "o4-mini",
+            -- choices = { ["o4-mini"] = { opts = { can_reason = true } }, },
+
+            -- default = "gpt-4o",
+            -- choices = {
+            --   "gpt-4o",
+            --   -- ["o3-mini"] = { opts = { can_reason = true } },
+            --   -- "gpt-4o-mini",
+            -- }
+
           },
         },
       })
