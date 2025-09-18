dda1891f6cd3a24491b0ed90bd4e2a0b043dd910
Kuanju Chen <kuanju0919@yahoo.com>
2025-09-18 15:20:22 +0000

    neovim: codecompanion address deprecated config
    `adapters.<adapter_name>` and `adapters.opts` is deprecated, use
    `adapters.http.<adapter_name>` and `adapters.http.opts`
     instead.

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/codecompanion.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/codecompanion.lua
@@ -1,3 +1,8 @@
+--[[
+`adapters.<adapter_name>` and `adapters.opts` is deprecated, use `adapters.http.<adapter_name>` and `adapters.http.opts`
+ instead.
+--]]
+
 require("codecompanion").setup({
   log_level = "DEBUG",
   display = {
@@ -6,47 +11,49 @@ require("codecompanion").setup({
     }
   },
   adapters = {
-    azure_openai = function()
-      return require("codecompanion.adapters").extend("azure_openai", {
-        -- https://codecompanion.olimorris.dev/getting-started.html
-        -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/adapters/azure_openai.lua
-        env = {
+    http = {
+      azure_openai = function()
+        return require("codecompanion.adapters").extend("azure_openai", {
+          -- https://codecompanion.olimorris.dev/getting-started.html
+          -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/adapters/azure_openai.lua
+          env = {
 
-          -- gpt-5-mini
-          api_key = 'cmd:pas mks/services/openai-it/key2',
-          endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
-          api_version = "2025-04-01-preview",
+            -- gpt-5-mini
+            api_key = 'cmd:pas mks/services/openai-it/key2',
+            endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
+            api_version = "2025-04-01-preview",
 
-          -- -- o4-mini
-          -- api_key = 'cmd:pas mks/services/openai-it/key2',
-          -- endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
-          -- api_version = "2025-01-01-preview",
+            -- -- o4-mini
+            -- api_key = 'cmd:pas mks/services/openai-it/key2',
+            -- endpoint = "https://azureaipowersolutioncgllm-eastus2.openai.azure.com",
+            -- api_version = "2025-01-01-preview",
 
-          -- -- gpt-4o
-          -- api_key = 'cmd:pas mks/services/openai-it/key1',
-          -- endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com",
-          -- api_version = "2025-01-01-preview",
-
-        },
-        schema = {
-          model = {
-            default = "gpt-5-mini",
-            choices = { "gpt-5-mini" },
-
-            -- default = "o4-mini",
-            -- choices = { ["o4-mini"] = { opts = { can_reason = true } }, },
-
-            -- default = "gpt-4o",
-            -- choices = {
-            --   "gpt-4o",
-            --   -- ["o3-mini"] = { opts = { can_reason = true } },
-            --   -- "gpt-4o-mini",
-            -- }
+            -- -- gpt-4o
+            -- api_key = 'cmd:pas mks/services/openai-it/key1',
+            -- endpoint = "https://azureaipowersolutioncgllm.cognitiveservices.azure.com",
+            -- api_version = "2025-01-01-preview",
 
           },
-        },
-      })
-    end,
+          schema = {
+            model = {
+              default = "gpt-5-mini",
+              choices = { "gpt-5-mini" },
+
+              -- default = "o4-mini",
+              -- choices = { ["o4-mini"] = { opts = { can_reason = true } }, },
+
+              -- default = "gpt-4o",
+              -- choices = {
+              --   "gpt-4o",
+              --   -- ["o3-mini"] = { opts = { can_reason = true } },
+              --   -- "gpt-4o-mini",
+              -- }
+
+            },
+          },
+        })
+      end,
+    },
   },
   strategies = {
     chat = {
