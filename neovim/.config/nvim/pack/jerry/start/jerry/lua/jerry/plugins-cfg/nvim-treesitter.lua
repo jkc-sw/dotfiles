05b3432d76fb610f1ad9470d431c4c406f311b85
Kuanju Chen <kuanju0919@yahoo.com>
2026-01-18 02:05:35 +0000

    neovim: remove unused configs in nvim-treesitter

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/nvim-treesitter.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/plugins-cfg/nvim-treesitter.lua
@@ -1,15 +1,4 @@
 require('nvim-treesitter').setup {
   sync_install = false,
   auto_install = false,
-  context_commentstring = {
-    enable = false,
-  },
-  highlight = {
-    enable = true,
-    disable = { 'cpp' }
-  },
-  indent = {
-    enable = false,
-    disable = { 'python' }
-  }
 }
