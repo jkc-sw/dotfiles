267176d261b1fe1d1d579822d032f00f3664c8ac
Kuanju Chen <kuanju0919@yahoo.com>
2026-01-18 02:48:06 +0000

    neovim: remove treesitter automatic fold

--- neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/global-options.lua
+++ neovim/.config/nvim/pack/jerry/start/jerry/lua/jerry/global-options.lua
@@ -161,8 +161,9 @@ vim.api.nvim_create_autocmd("FileType", {
     pcall(function()
       vim.treesitter.start()
       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      vim.wo[0][0].foldmethod = 'expr'
+      -- -- This automatic fold is annoying
+      -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- vim.wo[0][0].foldmethod = 'expr'
     end)
   end,
 })
